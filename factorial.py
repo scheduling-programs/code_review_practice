218d933bfa89b82263075e8c298aab272f84858a
Abenezer <44743368+Aben-Bel@users.noreply.github.com>
2023-07-07 11:32:30 +0300

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    # Calculates the factorial of n
+    if n < 0:
+        return "Sorry, factorial does not exist for negative numbers"
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n)
+
+print(factorial(5)) # This should print 120
